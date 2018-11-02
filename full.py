54cc0fe1a15ed5666daef11eb92ab85867fb6032
Paul Karayan <pk@addepar.com>
2018-11-01 19:23:30 -0700

    full works but i dont get it

--- full.py
+++ full.py
@@ -27,11 +27,14 @@ SAMPLE_DATASET = """1988.21104821
 1377.8200091"""
 SAMPLE_OUTPUT = "KEKEP"
 
+INV_MASS_TABLE = dict(zip(MASS_TABLE.values(), MASS_TABLE.keys()))
+print INV_MASS_TABLE
 
 def main(input_data):
     data = input_data.split("\n")
     parent_mass = float(data[0])
     ion_masses = map(float, data[1:])
+    print ion_masses
 
     # let's find the closest pair of ions - minimize their distance from 
     # parent mass. This means we'll have the b and y ion pairs to work with later.
@@ -51,17 +54,41 @@ def main(input_data):
     clean_pairs = [pair for pair in pairs if pair[0] < pair[1]]
     print clean_pairs
 
-    # remove pairs by smallest distance first
+    # iterate the list of pairs, trying to add an amino acid. 
+    # if it works, then add to sequence and remove from list
+    # if it doesn't work, flip b & y, add back and try again
     sequence = ''
+    while len(clean_pairs)>1:
+        suffix, prefix = clean_pairs[1][0], clean_pairs[0][0]
+        diff = suffix - prefix
+        
+        # close enough to 0?
+        # this neat min(  ) is getting the smallest difference.
+        closest_mass = min(MASS_TABLE.values(), key=lambda x: abs(x-diff))
+
+        if round(diff - closest_mass, 3) == 0:
+            sequence += INV_MASS_TABLE[closest_mass]
+            clean_pairs.pop(0)
+        else:
+            # flip the pairs and reinsert them
+            bad_pair = clean_pairs.pop(1)
+            clean_pairs = sorted(clean_pairs + [(bad_pair[1],bad_pair[0])])
+  
+    return sequence
 
 
-
+def get_key_by_value(value, dict_):
+    return (list(dict_.keys())[list(dict_.values()).index(value)])
 
 if __name__ == "__main__":
     ## Test
-    # main(SAMPLE_DATASET)
+    print main(SAMPLE_DATASET)
     assert main(SAMPLE_DATASET) == SAMPLE_OUTPUT
 
     ## Prod
-    #with open("./datasets/rosalind_XXX.txt", 'r') as fptr:
-        # dna = fptr.read().strip()
+    with open("./datasets/rosalind_full_1.txt", 'r') as fptr:
+        input_data = fptr.read().strip()
+        assert main(input_data) == "RKLPPFPNRTKQCLSQWNSHVSYELPLLNQHPLHMLDLNCLQEFTLLGCLLHHMPSEVYAQFVGSTLSEQWKCNQSCDTQLTLLAMP"
+    with open("./datasets/rosalind_full_1.txt", 'r') as fptr:
+        input_data = fptr.read().strip()
+        print main(input_data)
